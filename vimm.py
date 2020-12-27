899e19f3c5bbeeb6e86ce58574f5a236efbb4f86
lijimin12 <33054856+lijimin12@users.noreply.github.com>
2020-12-27 11:41:23 +0800

    initial commit of vimm.py

---
+++ vimm.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python
+
+import sys, string, os, subprocess
+
+#print(sys.argv)
+#print(sys.argv[0])
+
+help = "Usage: " + __file__ + " filename:line"
+
+if len(sys.argv) < 2:
+    print(help)
+    sys.exit(0)
+
+filename_line=sys.argv[1]
+#print(filename_line)
+
+first_blank=filename_line.find(' ')
+if (-1 != first_blank):
+    filename_line = filename_line[:first_blank]
+
+if filename_line[-1] == ':':
+    filename_line = filename_line[:-1]
+    #filename_line[-1] = ' '
+first_semicolon = filename_line.find(':')
+#print(first_semicolon)
+if (-1 != first_semicolon):
+    filename = filename_line[:first_semicolon]
+    line = filename_line[first_semicolon+1:]
+    second_semicolon = line.find(':')
+    if (-1 != second_semicolon):
+        line = line[:second_semicolon]
+    command = "vim " + filename + " +" + line
+else:
+    command = "vim " + filename_line
+
+#print(command)
+subprocess.call(command, shell=True)
+
