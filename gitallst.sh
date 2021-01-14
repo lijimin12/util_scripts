ed68a64e8e96dd92f6fc4bfa3077513cab001236
lijimin12 <33054856+lijimin12@users.noreply.github.com>
2021-01-14 13:32:00 +0800

    Update README, adding gitallst.sh

---
+++ gitallst.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# this prints out all local branches status
+
+# exit on error
+set -e
+# print before execute
+#set -x
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "current branch: $current_branch"
+echo "status:"
+git status -s
+echo "----------------------"
+#exit 0
+
+git for-each-ref --format="%(refname:short) %(upstream:short)" refs/heads | \
+while read local remote
+do
+#echo $local
+    if [ "$local" = "$current_branch" ] ; then
+        #echo "curent branch, continue"
+        continue
+    fi
+    git co $local
+    echo "status:"
+    git status -s
+    echo "----------------------"
+done
+
+git co $current_branch
+
